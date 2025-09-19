14307186a24bcbcce16a6b88ccca7f48682d37ed
HARI HARA CHANDAR B <112766702+HariHaraChandar0715@users.noreply.github.com>
2025-09-19 19:41:33 +0530

    Create column_transformation.py
    Added column transformation to the transformation package

---
+++ transformations/column_transformation.py
@@ -0,0 +1,11 @@
+def add_cloumns(df):
+    column_names = {
+        '_c0':'payment_id','_c1':'order_id',
+        '_c2':'payment_date','_c3':'payment_status',
+        '_c4':'payment_method'
+    }
+    for old_name, new_name in column_names.items():
+        df = df.withColumnRenamed(old_name,new_name)
+
+    return df
+
